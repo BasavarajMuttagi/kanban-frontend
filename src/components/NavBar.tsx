729f876e1f264ce52a123a7afb7ca0e8e728aa04
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-07-21 01:46:39 +0530

    add hover effect to login and signup navitem

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,19 +6,19 @@ const NavBar = () => {
     <nav className="bg-blue-500 h-16 flex items-center justify-between px-5 font-medium text-white tracking-wider">
       <NavLink to={"/"} className="flex items-center space-x-1.5">
         <h1 className="text-2xl">Kanban</h1>
-        <Notepad size={32} weight="fill" className="text-white" />
+        <Notepad size={32} weight="fill" />
       </NavLink>
       <ul className="flex items-center space-x-10">
-        <li className="group">
+        <li className="">
           <NavLink to={"/login"}>
-            <button className="px-3 py-1.5 rounded-md group-has-[>a.active]:bg-white group-has-[>a.active]:text-blue-500">
+            <button className="px-3 py-1.5 rounded-md hover:bg-white hover:text-blue-500  group-has-[>a.active]:bg-white group-has-[>a.active]:text-blue-500">
               Login
             </button>
           </NavLink>
         </li>
         <li className="group">
           <NavLink to={"/signup"}>
-            <button className="px-3 py-1.5 rounded-md group-has-[>a.active]:bg-white group-has-[>a.active]:text-blue-500">
+            <button className="px-3 py-1.5 rounded-md hover:bg-white hover:text-blue-500  group-has-[>a.active]:bg-white group-has-[>a.active]:text-blue-500">
               Signup
             </button>
           </NavLink>
