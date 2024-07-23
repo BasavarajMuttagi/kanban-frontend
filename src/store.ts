6540a115ed4aef16b1e9397afd43ed96ae7f5783
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-07-23 13:15:22 +0530

    create kanban store

---
+++ src/store.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+const storageModule = {
+  name: "kanban-board-storage",
+  storage: createJSONStorage(() => sessionStorage),
+};
+
+type store = {
+  token: string;
+  displayName: string;
+  setToken: (newToken: string) => void;
+  setDisplayName: (name: string) => void;
+  logout: () => void;
+  reset: () => void;
+};
+
+const useKanbanStore = create<store>()(
+  persist(
+    (set) => ({
+      token: "",
+      displayName: "",
+      setToken: (newToken) => set(() => ({ token: newToken })),
+      setDisplayName: (name: string) => set(() => ({ displayName: name })),
+      logout: () => {
+        set(() => ({
+          token: "",
+          displayName: "",
+        }));
+      },
+      reset: () => {
+        set(() => ({
+          token: "",
+          displayName: "",
+        }));
+      },
+    }),
+    storageModule
+  )
+);
+export default useKanbanStore;
